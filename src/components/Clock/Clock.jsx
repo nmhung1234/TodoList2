fac3a08fccd98a66d77834003cbb730ec8844b4e
nmhung1234 <nmhung1234@hotmail.com>
2021-03-26 17:08:23 +0700

    add weekdays

--- src/components/Clock/Clock.jsx
+++ src/components/Clock/Clock.jsx
@@ -1,15 +1,42 @@
 import React, { useState, useEffect, Fragment } from "react";
 
-import './style.css'
+import "./style.css";
+
 function formatTimeString(now) {
     let hours = `0${now.getHours()}`.slice(-2);
     let minutes = `0${now.getMinutes()}`.slice(-2);
     let seconds = `0${now.getSeconds()}`.slice(-2);
-    let date = now.getDate();
-    let month = now.getMonth() + 1;
+    let date = `0${now.getDate()}`.slice(-2);
+    let month = `0${now.getMonth() + 1}`.slice(-2);
     let year = now.getFullYear();
-
-    return `${hours}:${minutes}:${seconds} - ${date} / ${month} / ${year}`;
+    let current_day = now.getDay();
+    function convertDay(current_day) {
+        switch (current_day) {
+            case 0:
+                current_day = "Sunday";
+                break;
+            case 1:
+                current_day = "Monday";
+                break;
+            case 2:
+                current_day = "Tuesday";
+                break;
+            case 3:
+                current_day = "Wednesday";
+                break;
+            case 4:
+                current_day = "Thursday";
+                break;
+            case 5:
+                current_day = "Friday";
+                break;
+            case 6:
+                current_day = "Saturday";
+        }
+        return current_day;
+    }
+    let day = convertDay(current_day);
+    return `${hours}:${minutes}:${seconds} - ${day},  ${month}/${date}/${year}`;
 }
 
 function Clock() {
@@ -34,5 +61,3 @@ function Clock() {
 }
 
 export default Clock;
-
-
